86eccd6374116a37a7ea23a7c07e46757e7fcb29
Anar Chalk <annachak9@gmail.com>
2020-09-01 19:39:33 -0400

    added footer component

---
+++ src/Component/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer () {
+    return (
+        <footer id="main-footer" class="grid">
+        <article>
+          <small>&copy; 2020 Misha Coffee & Hookah</small>
+        </article>
+      </footer>
+    )
+}
