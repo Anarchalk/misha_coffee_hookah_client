09858eeba0f0d610aae0967834b98475ca020bb7
Anar Chalk <annachak9@gmail.com>
2020-09-03 12:37:28 -0400

    added hookah menu items

--- src/Component/Hookah/Hookah.js
+++ src/Component/Hookah/Hookah.js
@@ -21,9 +21,7 @@ const Hookah = () => {
       <section id="section-hookah" className="grid">
         <ul>
           <li>
-            <article className="card">
-              {/* <img className="small-pictures" src={brown} /> */}
-            </article>
+            <article className="card"></article>
             <article className="card-content">
               <h3 className="card-title menu">SHISHA</h3>
               <p id="price-name"></p>
@@ -145,46 +143,280 @@ const Hookah = () => {
             <article className="card"></article>
             <article className="card-content">
               <h3 className="card-title menu">VODKA Bottle 100 500</h3>
-              <li>
-                <p id="price-name">
-                  Grey Goose, France <span>₮ 190,0</span>{" "}
-                </p>
-              </li>
-              <li>
-                <p id="price-name">
-                  Eden <span>₮ 50,0</span>{" "}
-                </p>
-              </li>
-              <li>
-                <p id="price-name">
-                  Evok <span>₮ 60,0</span>{" "}
-                </p>
-              </li>
-              <li>
-                <p id="price-name">
-                  Velvet<span>₮ 60,0</span>{" "}
-                </p>
-              </li>
+              <ul>
+                <li>
+                  <p id="price-name">
+                    Grey Goose, France <span>₮ 190,0</span>{" "}
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Eden <span>₮ 50,0</span>{" "}
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Evok <span>₮ 60,0</span>{" "}
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Velvet<span>₮ 60,0</span>{" "}
+                  </p>
+                </li>
+              </ul>
             </article>
           </li>
           <li>
-            <article className="card">
-              {/* <img className="small-pictures" src={} /> */}
-            </article>
+            <article className="card"></article>
             <article className="card-content">
-              <h3 className="card-title menu">CUPCAKE</h3>
-              <p id="price">₮ 4900</p>
-              <p id="price-name"></p>
+              <h3 className="card-title menu">WHISKEY</h3>
+              <ul>
+                <li>
+                  <p id="price-name">
+                    Jack Daniel, Tennessee<span>₮ 150,0</span>
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Chivas Regal, Highland Blend<span>₮ 150,0</span>
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Ballantines, Scotland<span>₮ 120,0</span>
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Jameson, Ireland<span>₮ 110,0</span>
+                  </p>
+                </li>
+              </ul>
             </article>
           </li>
           <li>
-            <article className="card">
-              {/* <img className="small-pictures" src={} /> */}
-            </article>
+            <article className="card"></article>
             <article className="card-content">
-              <h3 className="card-title menu">HANDMADE CHOCO</h3>
-              <p id="price">₮ 4900</p>
-              <p id="price-name"></p>
+              <h3 className="card-title menu">SINGLE MALT WHISKEY</h3>
+              <p id="price-name">
+                Glen Moray, Scotland<span>₮ 150,0</span>
+              </p>
+            </article>
+          </li>
+          <li>
+            <article className="card"></article>
+            <article className="card-content">
+              <h3 className="card-title menu">COCKTAILS</h3>
+              <ul>
+                <li>
+                  <p id="price-name">
+                    Vodka Ice Cream<span>₮ 10,0</span>
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Mojito<span>₮ 10,0</span>
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Teqiula Sunrise<span>₮ 12,0</span>
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Long Island<span>₮ 15,0</span>
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Long Island Blue<span>₮ 15,0</span>
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Red Margaritta<span>₮ 12,0</span>
+                  </p>
+                </li>
+              </ul>
+            </article>
+          </li>
+          <li>
+            <article className="card"></article>
+            <article className="card-content">
+              <h3 className="card-title menu">NON-ALCOHOLIC BEVERAGES</h3>
+              <ul>
+                <li>
+                  <p id="price-name">
+                    Cola/Sprite/Fanta<span>₮ 3,0</span>
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Ginger Ale/Tonic/Soda<span>₮ 3,0</span>
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Khujirt Water<span>₮ 3,5</span>
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Bonaqua<span>₮ 2,0</span>
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Juice<span>₮ 4,0</span>
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Red Bull<span>₮ 7,0</span>
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    XS<span>₮ 7,0</span>
+                  </p>
+                </li>
+              </ul>
+            </article>
+          </li>
+          <li>
+            <article className="card"></article>
+            <article className="card-content">
+              <h3 className="card-title menu">HOT/ICED BEVERAGES</h3>
+              <ul>
+                <li>
+                  <p id="price-name">
+                    Sea Buckthorn<span>₮ 3,5</span>
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Red Huckelberry<span>₮ 3,5</span>
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Blueberry<span>₮ 3,5</span>
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Choco Milk<span>₮ 4,0</span>
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Honey Milk<span>₮ 3,5</span>
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Mango Punch Tea<span>₮ 4,9</span>
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Lipton<span>₮ 3,0</span>
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Green Tea<span>₮ 3,0</span>
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Pot Of Tea<span>₮ 3,0</span>
+                  </p>
+                </li>
+              </ul>
+            </article>
+          </li>
+          <li>
+            <article className="card"></article>
+            <article className="card-content">
+              <h3 className="card-title menu">MILKSHAKE ₮ 6,0</h3>
+              <ul>
+                <li>
+                  <p id="price-name">Chocolate</p>
+                </li>
+                <li>
+                  <p id="price-name">Matcha</p>
+                </li>
+                <li>
+                  <p id="price-name">Strawberry</p>
+                </li>
+                <li>
+                  <p id="price-name">Vanilla</p>
+                </li>
+                <li>
+                  <p id="price-name">Berry</p>
+                </li>
+              </ul>
+            </article>
+          </li>
+          <li>
+            <article className="card"></article>
+            <article className="card-content">
+              <h3 className="card-title menu">SMOOTHIE ₮ 7,0</h3>
+              <ul>
+                <li>
+                  <p id="price-name">Banana</p>
+                </li>
+                <li>
+                  <p id="price-name">Kiwi</p>
+                </li>
+                <li>
+                  <p id="price-name">Pineapple</p>
+                </li>
+                <li>
+                  <p id="price-name">Orange</p>
+                </li>
+              </ul>
+            </article>
+          </li>
+          <li>
+            <article className="card"></article>
+            <article className="card-content">
+              <h3 className="card-title menu">DESSERT/SNACKS</h3>
+              <ul>
+                <li>
+                  <p id="price-name">
+                    Cheesecake<span>₮ 4,9</span>
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Muffin<span>₮ 4,9</span>
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Waffle<span>₮</span>
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Chips<span>₮ 4,0</span>
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Nuts<span>₮ 4,0</span>
+                  </p>
+                </li>
+                <li>
+                  <p id="price-name">
+                    Fries<span>₮ 5,0</span>
+                  </p>
+                </li>
+              </ul>
             </article>
           </li>
         </ul>
